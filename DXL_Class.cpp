b8a2f412de0b3b5c951a39d73bff45374ce132bd
minimirror1 <minimirror1@gmail.com>
2023-12-13 14:59:45 +0900

    0.0.1v_기본정보입력

--- DXL_Class.cpp
+++ DXL_Class.cpp
@@ -5,6 +5,13 @@
 
 /* Motor Class 상속 */
 /* input 필수 기능 */
+//init
+void DXL_motor::setSettingInfo(uint8_t dir, uint16_t angle, uint16_t initPosi, uint16_t reducer_ratio)
+{
+
+}
+
+//control
 void DXL_motor::setPosition(uint16_t targetPosition)
 {
 
--- DXL_Class.h
+++ DXL_Class.h
@@ -1,22 +1,24 @@
 #include "cpp_Motor_Class_v2/Motor.h"
 
-
 class DXL_motor : public Motor {
 public:
-    DXL_motor(uint8_t gID, uint8_t sID) {
+    DXL_motor(uint8_t gID, uint8_t sID, MotorType motorType) :
+    	Motor(gID, sID, motorType){
         // Set IDs and other parameters for DXL_motor
-        gID_ = gID;
-        sID_ = sID;
     }
 
-    // Override the virtual function from the base class
-    virtual void doSomething() {
-        // Implement DXL_motor specific functionality
-        std::cout << "DXL_motor is doing something." << std::endl;
-    }
+    /* Motor Class 상속 */
+    /* input 필수 기능 */
+    //init
+    void setSettingInfo(uint8_t dir, uint16_t angle, uint16_t initPosi, uint16_t reducer_ratio);
 
-    // Add any other specific functions for DXL_motor
+    //control
+    void setPosition(uint16_t targetPosition);
+
+    /* output 필수 기능*/
+    uint16_t getPosition() const;//const 상태변경x 읽기전용
+
 
 private:
-    // Add any DXL_motor specific members if needed
+
 };
